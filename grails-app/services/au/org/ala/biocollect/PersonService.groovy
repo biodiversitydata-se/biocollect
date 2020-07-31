46f592911a711a42c4f18d3120681f437b151c9a
magdziarek <aleksandra.magdziarek@biol.lu.se>
2020-07-31 18:28:35 +0200

    remove redundant

--- grails-app/services/au/org/ala/biocollect/PersonService.groovy
+++ grails-app/services/au/org/ala/biocollect/PersonService.groovy
@@ -6,13 +6,12 @@ class PersonService {
     PersonService PersonService
 
     def create(body){
-        log.debug "person service"
         webService.doPost(grailsApplication.config.ecodata.baseURL + '/person/', body)
     }
 
     def getPersonsForProjectPerPage(String projectId){
-        log.debug "getPersonsForProjectPerPage"
         def url = grailsApplication.config.ecodata.baseURL + "/person/get/${projectId}"
-        webService.getJson(url)
+        def result = webService.getJson(url)
+        return result
     }
 }
