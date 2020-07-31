29734dc6910911a7bc0a16764b6899aa6b7c85bd
magdziarek <aleksandra.magdziarek@biol.lu.se>
2020-07-31 13:35:30 +0200

    add get method

--- grails-app/controllers/au/org/ala/biocollect/PersonController.groovy
+++ grails-app/controllers/au/org/ala/biocollect/PersonController.groovy
@@ -1,30 +1,75 @@
 package au.org.ala.biocollect
 
-import au.org.ala.biocollect.merit.PreAuthorise
-import au.org.ala.biocollect.merit.UserService
 import au.org.ala.biocollect.PersonService
+import au.org.ala.biocollect.merit.UserService
+import au.org.ala.biocollect.merit.ProjectService
+
 import grails.converters.JSON
-import org.apache.http.HttpStatus
 
 class PersonController {
 
+    PersonService personService 
     UserService userService
-    PersonService personService
+    ProjectService projectService
 
-    def ajaxCreate(String id) {
-        log.debug "ajax update person"
+    def ajaxCreate() {
 
         def values = request.JSON
-        log.debug "values: " + (values as JSON).toString()
-
         Map result = personService.create(values)
-        log.debug "result" + result
+        log.debug "result of person service call " + result
 
         if (result.error) {
             response.status = 500
         } else {
-            log.debug "no result.error"
             render result as JSON
+        }
+    }
+
+    def ajaxList(String id) {
+//         if(params.entityType == "projectActivity") {
+            def pActivity = projectActivityService.get(id, 'all')
+    //    def sites = siteService.getSitesFromIdList(pActivity.sites, BRIEF)
+//             if (!pActivity) {
+//                 response.sendError(404, "Couldn't find project activity $id")
+//                 return
+//             }
+//             log.info("sites for activity" + pActivity.sites.toString())
+//             render pActivity.sites as JSON
+
+//         } else if (params.entityType == "project") {
+//             def project = projectService.get(id, "all")
+//             if (!project) {
+//                 response.sendError(404, "Couldn't find project $id")
+//                 return
+//             }
+
+//             render project?.sites  as JSON
+//         }
+
+    }
+
+    def getPersonsForProjectIdPaginated() {
+        log.debug "inside getPersonsForProjectIdPaginated params are" + params
+        String projectId = params.id
+        log.debug "Project ID " + projectId
+        def adminUserId = userService.getCurrentUserId()
+
+        if (projectId && adminUserId) {
+            log.debug "you are admin"
+            if (projectService.isUserAdminForProject(adminUserId, projectId) || projectService.isUserCaseManagerForProject(adminUserId, projectId)) {
+                def results = personService.getPersonsForProjectPerPage(projectId)
+                log.debug "results " + results
+                asJson results
+
+            } else {
+                response.sendError(SC_FORBIDDEN, 'Permission denied')
+            }
+        } else if (adminUserId) {
+            response.sendError(SC_BAD_REQUEST, 'Required params not provided: id')
+        } else if (projectId) {
+            response.sendError(SC_FORBIDDEN, 'User not logged-in or does not have permission')
+        } else {
+            response.sendError(SC_INTERNAL_SERVER_ERROR, 'Unexpected error')
         }
     }
 
@@ -33,8 +78,7 @@ class PersonController {
 
     }
     def list() {
-        render view: 'create', model: [create:true]
-
+        log.debug "list in person service"
     }
 
     // def edit(String id) {
@@ -47,31 +91,4 @@ class PersonController {
 
     // }
 
-
-    // Throw-away
-    // def create() {
-    //     respond new Person(params)
-    // }
-
-    // def save(Person person) {
-    //     if (person == null) {
-    //         notFound()
-    //         return
-    //     }
-
-    //     try {
-    //         personService.save(person)
-    //     } catch (ValidationException e) {
-    //         respond person.errors, view:'create'
-    //         return
-    //     }
-
-    //     request.withFormat {
-    //         form multipartForm {
-    //             flash.message = message(code: 'default.created.message', args: [message(code: 'person.label', default: 'Person'), person.id])
-    //             redirect person
-    //         }
-    //         '*' { respond person, [status: CREATED] }
-    //     }
-    // } 
 }
