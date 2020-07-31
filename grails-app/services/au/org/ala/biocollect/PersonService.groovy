6935192437b279f2427a50785375fb56c003fcb0
magdziarek <aleksandra.magdziarek@biol.lu.se>
2020-07-31 13:35:46 +0200

    add get method

--- grails-app/services/au/org/ala/biocollect/PersonService.groovy
+++ grails-app/services/au/org/ala/biocollect/PersonService.groovy
@@ -6,7 +6,13 @@ class PersonService {
     PersonService PersonService
 
     def create(body){
+        log.debug "person service"
         webService.doPost(grailsApplication.config.ecodata.baseURL + '/person/', body)
     }
 
+    def getPersonsForProjectPerPage(String projectId){
+        log.debug "getPersonsForProjectPerPage"
+        def url = grailsApplication.config.ecodata.baseURL + "/person/get/${projectId}"
+        webService.getJson(url)
+    }
 }
