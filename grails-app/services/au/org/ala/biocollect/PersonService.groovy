787dc9b05ed161b56c9a9a8bba20017587beec77
magdziarek <aleksandra.magdziarek@biol.lu.se>
2020-08-04 12:44:06 +0200

    add delete and update methods

--- grails-app/services/au/org/ala/biocollect/PersonService.groovy
+++ grails-app/services/au/org/ala/biocollect/PersonService.groovy
@@ -3,15 +3,31 @@ package au.org.ala.biocollect
 class PersonService {
 
     def webService, grailsApplication
-    PersonService PersonService
+    PersonService personService
 
     def create(body){
-        webService.doPost(grailsApplication.config.ecodata.baseURL + '/person/', body)
+        webService.doPost(grailsApplication.config.ecodata.baseURL + '/person/', body) 
+    }
+
+    def update(String id){
+        log.debug params
     }
 
     def getPersonsForProjectPerPage(String projectId){
         def url = grailsApplication.config.ecodata.baseURL + "/person/get/${projectId}"
         def result = webService.getJson(url)
         return result
+    }
+
+    def delete(String personId){
+        log.debug "delete person - serv"
+        def response = webService.doDelete(grailsApplication.config.ecodata.baseURL + '/person/' + personId)
+
+        // TODO should someone be notified when a person is deleted?
+        // String personName = get(id, "brief")?.lastName
+        // String subject = "Pesron wtith last name ${personName ?: id} was deleted by ${userService.currentUserDisplayName}"
+        // String emailBody = "User ${userService.currentUserId} (${userService.currentUserDisplayName}) has deleted project ${personName ?: id}"
+        // emailService.sendEmail(subject, emailBody, ["${grailsApplication.config.biocollect.support.email.address}"])
+        response
     }
 }
