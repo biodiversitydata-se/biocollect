ce6dca1a5cd597adeb6c114951dc91be64adbb5b
magdziarek <aleksandra.magdziarek@biol.lu.se>
2021-04-09 13:58:54 +0200

    remove faulty facets

--- grails-app/controllers/au/org/ala/biocollect/merit/SiteController.groovy
+++ grails-app/controllers/au/org/ala/biocollect/merit/SiteController.groovy
@@ -915,7 +915,7 @@ class SiteController {
             }
 
             if (!queryParams.facets) {
-                queryParams.facets = "typeFacet,className,organisationFacet,stateFacet,lgaFacet,nrmFacet,siteSurveyNameFacet,siteProjectNameFacet,photoType,isBooked,kartaTxFacet,verificationStatusFacet,LSKFacet,LANFacet"
+                queryParams.facets = "typeFacet,className,organisationFacet,stateFacet,lgaFacet,nrmFacet,siteSurveyNameFacet,siteProjectNameFacet,photoType,isBooked,verificationStatusFacet"
             }
             if (queryParams.query) {
                 query.push(queryParams.query);
@@ -985,8 +985,6 @@ class SiteController {
                         bookedBy         : doc?.bookedBy,
                         kartaTx          : doc?.kartaTx,
                         verificationStatus : doc?.verificationStatus,
-                        LSK : doc?.LSK,
-                        LAN : doc?.LAN,
                         // does a logical OR reduce operation on permissions for each projects
                         canEdit             : canEdit,
                         // only sites with no projects can be deleted
