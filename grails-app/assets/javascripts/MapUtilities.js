539b370e565021f92a5c147a44fa0fe36100c387
Mathieu Blanchet <mathieu.blanchet@biol.lu.se>
2025-08-06 12:25:06 +0200

    add login LM API with basic auth

--- grails-app/assets/javascripts/MapUtilities.js
+++ grails-app/assets/javascripts/MapUtilities.js
@@ -159,7 +159,6 @@ Biocollect.MapUtilities = {
      * @returns {L.tileLayer | L.Google}
      */
     getBaseLayer: function (code) {
-        var token = "8e55e105-71ff-3816-98eb-86f7979c3650";
         var option, layer;
         switch (code) {
             case 'minimal':
@@ -239,14 +238,20 @@ Biocollect.MapUtilities = {
                 layer = new L.Google('TERRAIN', {uniqueName: 'googleterrain', maxZoom: 21, nativeMaxZoom: 21});
                 break;
             case 'lantmateriettopo':
+                //var token = "8e55e105-71ff-3816-98eb-86f7979c3650";
+                var LMusername = "lunu0009";
+                var LMpassword = "Gi2R2I3jBl51wx8hZAFW";
+                
                 option = {
                     // see https://opendata.lantmateriet.se/#apis?api=OpenDataWMTS&version=v1
-                    url: 'https://api.lantmateriet.se/open/topowebb-ccby/v1/wmts/token/'+ token +'/?SERVICE=WMTS&REQUEST=GetTile&VERSION=1.0.0&LAYER=topowebb&STYLE=default&TILEMATRIXSET=3857&TILEMATRIX={z}&TILEROW={y}&TILECOL={x}&FORMAT=image%2Fpng',
+                    //url: 'https://api.lantmateriet.se/open/topowebb-ccby/v1/wmts/token/'+ token +'/?SERVICE=WMTS&REQUEST=GetTile&VERSION=1.0.0&LAYER=topowebb&STYLE=default&TILEMATRIXSET=3857&TILEMATRIX={z}&TILEROW={y}&TILECOL={x}&FORMAT=image%2Fpng',
+                    url: 'https://' + LMusername + ':' + LMpassword + '@maps.lantmateriet.se/open/topowebb-ccby/v1/wmts?SERVICE=WMTS&REQUEST=GetTile&VERSION=1.0.0&LAYER=topowebb&STYLE=default&TILEMATRIXSET=3857&TILEMATRIX={z}&TILEROW={y}&TILECOL={x}&FORMAT=image%2Fpng',
                     options: {
                         attribution: '&copy; <a href="https://www.lantmateriet.se/en/">Lantmäteriet</a> Topografisk Webbkarta Visning, CCB'
                     }
                 };
                 layer = L.tileLayer(option.url, option.options);
+                
                 break;
         }
 
